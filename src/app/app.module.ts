f7fcbd3bad6a53db47dcca71996cd3c6dde433df
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-06-21 21:29:15 +0200

    Removed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CoreModule } from 'src/app/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +7,7 @@ import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   declarations: [AppComponent, CallbackComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NgbModule.forRoot()],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
