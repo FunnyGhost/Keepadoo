41b8a05444433e743d5832e285ff8206504b3de2
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-06-23 20:06:41 +0200

    Implemented movie component

--- src/app/movie-lists/movie-list/movie-list.module.ts
+++ src/app/movie-lists/movie-list/movie-list.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { MovieListRoutingModule } from './movie-list-routing.module';
 import { MovieListComponent } from './movie-list.component';
 
 @NgModule({
-  imports: [CommonModule, MovieListRoutingModule],
+  imports: [CommonModule, MovieListRoutingModule, SharedModule],
   declarations: [MovieListComponent]
 })
 export class MovieListModule {}
--- src/app/movie-lists/shared/shared.module.ts
+++ src/app/movie-lists/shared/shared.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, MatCardModule, MatIconModule, FlexLayoutModule, MatButtonModule],
+  declarations: [MovieComponent],
+  exports: [MovieComponent, FlexLayoutModule]
 })
-export class SharedModule { }
+export class SharedModule {}
