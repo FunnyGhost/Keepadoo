249040ea2071d02a46e50687d780cf83e70f8ad1
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-06-23 20:12:14 +0200

    Fixed tests

--- src/app/movie-lists/shared/movie/movie.component.spec.ts
+++ src/app/movie-lists/shared/movie/movie.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
+import { Movie } from '../../core/models/movie';
 import { MovieComponent } from './movie.component';
+
+const movieToUse: Movie = {
+  key: 'some-key-here',
+  title: 'The laughing joke',
+  vote_average: 5,
+  vote_count: 9999,
+  overview: 'Crazinest at its best',
+  poster_path: 'some-path-here'
+};
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
@@ -8,14 +18,15 @@ describe('MovieComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovieComponent ]
-    })
-    .compileComponents();
+      imports: [MatCardModule, MatButtonModule, MatIconModule],
+      declarations: [MovieComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieComponent);
     component = fixture.componentInstance;
+    component.movie = movieToUse;
     fixture.detectChanges();
   });
 
