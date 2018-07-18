95c042c7ee3d3352d8df387c6d963f0f628cd139
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-18 12:20:04 +0200

    The callback component is now dumb

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,15 +1,10 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { AuthenticationService } from '../core/authentication.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kpd-callback',
   templateUrl: './callback.component.html',
   styleUrls: ['./callback.component.scss']
 })
-export class CallbackComponent implements AfterViewInit {
-  constructor(private authService: AuthenticationService) {}
-
-  ngAfterViewInit() {
-    this.authService.continueFromWhereYouLeftOff();
-  }
+export class CallbackComponent {
+  constructor() {}
 }
