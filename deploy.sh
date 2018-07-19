5ccdeda208f539fe4bb473f4df6af12a49847248
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-19 13:06:54 +0200

    Execute file against remote

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-sshpass  -p $1 ssh StrictHostKeyChecking=no root@174.138.8.175 <<-'ENDSSH'
    docker stop keepadoo
    docker rm keepadoo
    docker pull utukku/keepadoo:1.0
    docker run --name keepadoo -p 80:80 -d utukku/keepadoo:1.0
-ENDSSH
