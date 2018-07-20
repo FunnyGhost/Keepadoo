e40f859d79534d4f8b29ddea4f0bdf1e1389f7f8
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-20 17:15:02 +0200

    TvShows and Movies without posters are filtered out from search

--- src/app/core/tmdb.service.spec.ts
+++ src/app/core/tmdb.service.spec.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpRequest } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
-import { environment } from '../../environments/environment';
-import { TMDBService } from './tmdb.service';
 import { MovieSearchResult } from 'src/app/movie-list/core/models/movie-search-result';
+import { environment } from '../../environments/environment';
+import { TvShowSearchResult } from '../tv-show-lists/core/models/tv-show-search-result';
+import { TMDBService } from './tmdb.service';
 
 describe('TMDBService', () => {
   let httpClient: HttpClient;
@@ -54,6 +55,49 @@ describe('TMDBService', () => {
     httpTestingController.verify();
   }));
 
+  it('should filter movie search results without a poster from tmdb', done => {
+    inject([TMDBService], (service: TMDBService) => {
+      const searchText = 'Thor';
+      const dataToReturn = {
+        results: [
+          {
+            title: 'Thor',
+            overview: 'Something about a greek god..',
+            poster_path: 'thor-poster'
+          } as MovieSearchResult,
+          {
+            title: 'Iron man',
+            overview: 'Something about a dude with an iron suit..',
+            poster_path: 'iron-man-poster'
+          } as MovieSearchResult,
+          {
+            title: 'Chinese Iron Man',
+            overview: 'Something about a chinese dude with an iron suit..',
+            poster_path: ''
+          } as MovieSearchResult
+        ]
+      };
+
+      service.searchMovies(searchText).subscribe((searchResults: MovieSearchResult[]) => {
+        expect(searchResults).toEqual([dataToReturn.results[0], dataToReturn.results[1]]);
+        done();
+      });
+
+      const req = httpTestingController.expectOne((request: HttpRequest<any>) => {
+        const containsUrl = request.url.includes(environment.tmdbConfig.apiUrl);
+        const containsKey = request.params.get('api_key') === environment.tmdbConfig.api_key;
+        const containesSearchQuery = request.params.get('query') === searchText;
+        return containsUrl && containsKey && containesSearchQuery;
+      });
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(dataToReturn);
+
+      httpTestingController.verify();
+    })();
+  });
+
   it('should get tv search results from tmdb', inject([TMDBService], (service: TMDBService) => {
     const searchText = 'Vikings';
     const dataToReturn = {
@@ -81,4 +125,47 @@ describe('TMDBService', () => {
 
     httpTestingController.verify();
   }));
+
+  it('should filter tv search results without a poster from tmdb', done => {
+    inject([TMDBService], (service: TMDBService) => {
+      const searchText = 'Vikings';
+      const dataToReturn = {
+        results: [
+          {
+            name: 'Vikings',
+            overview: 'Something about vikings..',
+            poster_path: 'vikings-poster'
+          } as TvShowSearchResult,
+          {
+            name: 'Preacher',
+            overview: 'Something about a dude with a voice..',
+            poster_path: 'preacher-poster'
+          } as TvShowSearchResult,
+          {
+            name: 'Tzu-thing-chang',
+            overview: 'Something chinese...',
+            poster_path: ''
+          } as TvShowSearchResult
+        ]
+      };
+
+      service.searchTvShows(searchText).subscribe((data: TvShowSearchResult[]) => {
+        expect(data).toEqual([dataToReturn.results[0], dataToReturn.results[1]]);
+        done();
+      });
+
+      const req = httpTestingController.expectOne((request: HttpRequest<any>) => {
+        const containsUrl = request.url.includes(environment.tmdbConfig.apiUrl);
+        const containsKey = request.params.get('api_key') === environment.tmdbConfig.api_key;
+        const containesSearchQuery = request.params.get('query') === searchText;
+        return containsUrl && containsKey && containesSearchQuery;
+      });
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(dataToReturn);
+
+      httpTestingController.verify();
+    })();
+  });
 });
