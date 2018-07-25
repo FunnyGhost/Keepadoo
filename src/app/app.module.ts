b7c100f6dbbca02c7434654e6f4dcd3c976d6697
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-25 23:25:10 +0200

    Added ReduxDevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CallbackComponent } from './callback/callback.component';
@@ -10,7 +12,18 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [AppComponent, CallbackComponent, SidenavComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    StoreDevtoolsModule.instrument({
+      name: 'Keepadoo Devtools',
+      maxAge: 25,
+      logOnly: environment.production
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
