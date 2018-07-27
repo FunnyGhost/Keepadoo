63ae133b408be58dfee2a4a89029dbd3cf29ca4e
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-27 21:54:43 +0200

    The sidebar now uses the store

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { MovieList } from '../movie-list/core/models/movie-list';
-import { MovieListsService } from '../movie-list/core/movie-lists.service';
+import * as selectors from '../state/state';
+import { UserState } from '../state/state';
 
 @Component({
   selector: 'kpd-sidenav',
@@ -11,41 +13,30 @@ import { MovieListsService } from '../movie-list/core/movie-lists.service';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-  private _isAuthenticated: boolean;
-  @Input()
-  get isAuthenticated(): boolean {
-    return this._isAuthenticated;
-  }
-  set isAuthenticated(value: boolean) {
-    this._isAuthenticated = value;
-    if (!this._isAuthenticated) {
-      this.showMovieLists = false;
-    }
-  }
-
   showMovieLists: boolean;
-
   movieLists$: Observable<MovieList[]>;
 
-  constructor(private movieListsService: MovieListsService, private router: Router) {}
+  constructor(private userStore: Store<UserState>, private router: Router) {}
 
   ngOnInit() {
-    this.movieLists$ = this.movieListsService.getMovieLists().pipe(
+    this.setupRouteListening();
+
+    this.movieLists$ = this.userStore.pipe(select(selectors.getMovieLists)).pipe(
+      tap((data: MovieList[]) => console.log('movie lists', data)),
       filter(() => this.showMovieLists),
-      filter((data: MovieList[]) => data.length > 0),
       tap((data: MovieList[]) => {
-        this.router.navigate(['movie-lists', data[data.length - 1].key]);
+        if (data.length > 0) {
+          this.router.navigate(['movie-lists', data[data.length - 1].key]);
+        }
       })
     );
-
-    this.setupRouteListening();
   }
 
   private setupRouteListening() {
     this.router.events
       .pipe(filter(e => e instanceof RouterEvent && e instanceof NavigationEnd))
       .subscribe((e: RouterEvent) => {
-        if (this.isAuthenticated && e.url.includes('movie-lists')) {
+        if (e.url.includes('movie-lists')) {
           this.showMovieLists = true;
         }
       });
