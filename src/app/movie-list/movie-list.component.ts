96fc677ad2c98eaaeb598ba1681f690bb960dc5e
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-27 22:06:19 +0200

    MovieListComponent now uses the store

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { ModalService } from '../core/modal.service';
 import { ConfirmDeleteComponent } from '../shared/modals/confirm-delete/confirm-delete.component';
 import { NewListComponent } from '../shared/modals/new-list/new-list.component';
+import * as userSelectors from '../state/state';
+import { UserState } from '../state/state';
 import { MovieList } from './core/models/movie-list';
 import { MovieListsService } from './core/movie-lists.service';
+import * as movieActions from './state/movie.action';
+import * as movieSelectors from './state/movie.state';
+import { MovieState } from './state/movie.state';
 
 @Component({
   selector: 'kpd-movie-list',
@@ -18,23 +24,32 @@ export class MovieListComponent implements OnInit {
 
   constructor(
     private movieListsService: MovieListsService,
+    private userStore: Store<UserState>,
+    private movieStore: Store<MovieState>,
     private modalService: ModalService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.movieList$ = this.route.paramMap.pipe(
-      map((params: ParamMap) => {
-        return params.get('id');
-      }),
-      switchMap((listId: string) => {
-        return this.movieListsService.getMovieLists().pipe(
-          map((movieLists: MovieList[]) => {
-            return movieLists.find((movieList: MovieList) => movieList.key === listId);
-          })
-        );
-      })
-    );
+    this.movieList$ = this.movieStore.pipe(select(movieSelectors.getCurrentList));
+
+    this.route.paramMap
+      .pipe(
+        map((params: ParamMap) => {
+          return params.get('id');
+        }),
+        switchMap((listId: string) => {
+          return this.userStore.pipe(select(userSelectors.getMovieLists)).pipe(
+            map((movieLists: MovieList[]) => {
+              return movieLists.find((movieList: MovieList) => movieList.key === listId);
+            }),
+            tap((movieList: MovieList) => {
+              this.movieStore.dispatch(new movieActions.SelectMovieList(movieList));
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
   addList() {
