542840c8279a180e4803623fcb8655af553211e7
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-08-20 17:00:09 +0200

    Added jest coverage

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,19 @@ module.exports = {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
     '@env': '<rootDir>/src/environments/environment',
     '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1',
+    '@state/(.*)': '<rootDir>/src/app/state/$1'
   },
+  collectCoverageFrom: [
+    'src/app/**/*.{ts}',
+    '!src/app/*.{ts}',
+    '!src/app/**/*.{js}',
+    '!src/app/environment/*.{ts}',
+    '!src/app/language/*.{ts}',
+    '!src/app/**/*.module.{ts}',
+    '!src/app/**/*.interface.{ts}',
+    '!src/app/**/*.state.{ts}',
+    '!src/app/**/*.entity.{ts}'
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', 'src/app/*.{js}'],
+  testResultsProcessor: 'jest-sonar-reporter'
 };
