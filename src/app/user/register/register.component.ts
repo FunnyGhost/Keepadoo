830099e59c46d0d31dd6a2e8d8e29e192c9305ba
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-08-26 19:36:36 +0200

    Created Register Component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationModel } from '../../authentication.model';
+import { AuthenticationService } from '../../core/authentication.service';
 
 @Component({
   selector: 'kpd-register',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  register() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService.register(this.registerForm.value as AuthenticationModel).then(
+      res => {
+        this.errorMessage = '';
+      },
+      err => {
+        this.errorMessage = err.message;
+      }
+    );
+  }
 }
