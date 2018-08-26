c20b4dc7747f8df5529eeb176b25d0920d79ea9c
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-08-26 15:35:37 +0200

    Register component Genesis

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [CommonModule, UserRoutingModule],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, RegisterComponent]
 })
 export class UserModule {}
