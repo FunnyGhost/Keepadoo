94765c4cd47aeeef46fe53dfc6ec18651c7ee7a2
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-08-26 19:36:54 +0200

    AuthenticationService now can login and register

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
-import { UserRoutingModule } from './user-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [CommonModule, UserRoutingModule],
-  declarations: [ProfileComponent, RegisterComponent]
+  imports: [CommonModule, ReactiveFormsModule, UserRoutingModule],
+  declarations: [ProfileComponent, RegisterComponent, LoginComponent]
 })
 export class UserModule {}
