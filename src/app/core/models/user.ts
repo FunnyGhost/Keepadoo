8ed0da627cfc8fe293e349cf008fc38f792d9b97
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-08-27 09:38:04 +0200

    Cleaned UserModel

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,11 +1,4 @@
 export interface User {
-  email_verified: boolean;
+  userId: string;
   email: string;
-  updated_at: Date;
-  name: string;
-  picture: string;
-  user_id: string;
-  nickname: string;
-  created_at: Date;
-  sub: string;
 }
