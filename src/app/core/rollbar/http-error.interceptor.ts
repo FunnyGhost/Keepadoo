192c325f2043f97defeb1dcd7837e3a3dccdd847
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-12-13 10:55:31 +0100

    Added tests to the interceptor

--- src/app/core/rollbar/http-error.interceptor.ts
+++ src/app/core/rollbar/http-error.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { RollbarService } from './rollbar.service';
 
 @Injectable({ providedIn: 'root' })
@@ -16,7 +16,6 @@ export class HttpErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
-      retry(1),
       catchError((error: HttpErrorResponse) => {
         const rollbar = this.injector.get(RollbarService);
         let errorMessage = '';
