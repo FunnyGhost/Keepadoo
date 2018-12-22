a6a5b4f1a6d88d7f381f63a7c7e1e9653e446881
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-12-22 13:24:28 +0100

    Added path for the movie-details component

--- src/app/movie-list/movie-list-routing.module.ts
+++ src/app/movie-list/movie-list-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MovieListComponent } from './movie-list.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: MovieListComponent
+    path: 'movies/:id',
+    component: MovieDetailsComponent
   },
   {
     path: ':id',
